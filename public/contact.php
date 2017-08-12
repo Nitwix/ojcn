a26af4ab9ac2feafb9bc0f46a323c74b699dd268
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-12 10:54:22 +0200

    suppression de info@ojcn.ch dans contact.php

--- public/contact.php
+++ public/contact.php
@@ -7,7 +7,7 @@
 
 		<div class="container text-center">    
 			<h1>Une question? Une requête?</h1>
-			<h3>Contactez-nous simplement à <a href="mailto:info@ojcn.ch">info@ojcn.ch</a> ou à <a href="mailto:ojcn.officiel@gmail.com">ojcn.officiel@gmail.com</a></h3>
+			<h3>Contactez-nous simplement à <a href="mailto:ojcn.officiel@gmail.com">ojcn.officiel@gmail.com</a></h3>
 		</div>
 
 		<?php include "../includes/footer.php"?>
