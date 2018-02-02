f1650dd84d7ead77ef7f3090bb9693c316a43233
Niels Lachat <nielsnfsmw@gmail.com>
2018-02-02 20:05:44 +0100

    - Ajout de la section échange OJHB dans manifestations - changement des photos dans Accueil et Prochaines manifestations

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 				<div class='col-sm-2 col-lg-3 col-md-2'></div>
 				<div class='col-sm-8 col-lg-6 col-md-8'>
 					<img class='img-responsive' width='100%' src='../images/footer_cmne.png'>
-					<p class='text-center'>Copyright © Niels Lachat 2018 - Dernière mise à jour : 11.01.2018</p>
+					<p class='text-center'>Copyright © Niels Lachat 2018 - Dernière mise à jour : 02.02.2018</p>
 				</div>
 				<div class='col-sm-2 col-lg-3 col-md-2'></div>
 			</div>
