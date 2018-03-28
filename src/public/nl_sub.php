eb3baee83259d622c0eaa943076f84e14e981a03
Niels Lachat <nielsnfsmw@gmail.com>
2018-03-28 21:42:29 +0200

    début du travail sur le mail de confirmation

---
+++ src/public/nl_sub.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include "../includes/head.php"?>
+	<body>
+		<?php include "includes/navbar.php"?>
+
+		<?php
+			$purpose = htmlspecialchars($_POST["purpose"]);
+			$email = htmlspecialchars($_POST["email"]);
+			
+			//require "../sensible/db_connect.php";
+			//se connecter à la db pour voir si l'email n'existe pas déjà
+
+			//si l'email n'existe pas, envoyer un mail de confirmation et ajouter à la db
+			//sinon, afficher que le mail existe déjà
+
+			class Contents{
+				public $title;
+				public $subtitle;
+				public $email;
+
+
+				function __construct($purpose, $email){
+					$this->email = $email;
+					switch($purpose){
+						case "verify_msg":
+							$this->title = "Veuillez confirmer votre email.";
+							$this->subtitle = "Un e-mail de confirmation vous a été envoyé à l'adresse suivante: $this->email";
+							break;
+						default:
+							echo "<div class='container' style='font-size:50px'>Error: Purpose not found...</div>";
+					}
+				}
+
+				public function display(){
+					$out = "";
+					$out .= "<h1>$this->title</h1>";
+					$out .= "<h3>$this->subtitle<h3>";
+					echo $out;
+				}
+			}
+
+			//contenus de la page
+			$contents = new Contents($purpose, $email);
+		?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2 col-lg-2"></div>
+				<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+					<?php
+						$contents->display();
+					?>
+				</div>
+				<div class="col-md-2 col-lg-2"></div>
+			</div>
+		</div>
+
+		<?php include "../includes/footer.php"?>
+	</body>
+</html>
+
